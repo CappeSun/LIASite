4a4e82ec5330e763adff0f05ba4b5e899e0b5348
Cappe <casper@sundegard.st>
2024-03-18 12:39:36 +0100

    bp

---
+++ LIASite/resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LIASite</title>
+</head>
+<body>
+    <h1>Yup, seems to work</h1>
+</body>
+</html>
