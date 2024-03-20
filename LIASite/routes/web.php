cf0d93909d911b3a0db6b6e2369d6a11aec2cfb0
MylAnt95 <anton.myllykoski95@gmail.com>
2024-03-20 16:02:38 +0100

    Added groups to routes

--- LIASite/routes/web.php
+++ LIASite/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+// Guest users
+Route::middleware('guest')->group(function () {
+});
+
+// Authenticated users
+Route::middleware('auth')->group(function () {
+});
