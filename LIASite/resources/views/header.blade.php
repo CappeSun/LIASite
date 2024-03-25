66784408af847ac36286e112de227089160d066e
MylAnt95 <anton.myllykoski95@gmail.com>
2024-03-25 16:03:43 +0100

    Added blade files and some structure

---
+++ LIASite/resources/views/header.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+<header>
+    <nav>
+        <div class="logo">
+            <img src="" alt="">
+        </div>
+        <div class="dropdown-menu">
+            <img src="" alt="">
+        </div>
+    </nav>
+</header>
--- LIASite/resources/views/index.blade.php
+++ LIASite/resources/views/index.blade.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>LIASite</title>
-</head>
-<body>
-    <h1>Yup, seems to work</h1>
-</body>
-</html>
+@extends('layouts.app')
+@include('header')
+@section('content')
+    <div>
+        <h1>Welcome</h1>
+        <p>This is the homepage</p>
+    </div>
+@endsection
