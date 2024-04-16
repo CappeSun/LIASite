c2dac215f697fe4489b1edee466584c90e086cf4
Cappe <casper@sundegard.st>
2024-04-16 17:15:17 +0200

    create account fixed

--- LIASite/resources/views/register-student.blade.php
+++ LIASite/resources/views/register-student.blade.php
@@ -6,18 +6,17 @@
         <h6>Skapa ett konto</h6>
         {{-- TODO: Add routes --}}
         <form action="/account/create" method="post">
-            @csrf
             <label class="p1" for="name">Namn</label>
             <input class="form-input" type="text" id="name" name="name" placeholder="Namn..." value="{{ old('name') }}" required>
         
             <label class="p1" for="email">E-postadress</label>
-            <input class="form-input" type="email" id="studentEmail" name="email" placeholder="name@example.com" value="{{ old('email') }}" required>
+            <input class="form-input" type="email" id="email" name="email" placeholder="name@example.com" value="{{ old('email') }}" required>
         
             <label class="p1" for="password">Lösenord</label>
-            <input class="form-input" type="password" id="studentPassword" name="password" placeholder="Lösenord" value="{{ old('password') }}" required>
+            <input class="form-input" type="password" id="password" name="password" placeholder="Lösenord" value="{{ old('password') }}" required>
         
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <input type="hidden" name="level" value="0">
+            <input type="hidden" name="level" value="1">
             <button class="btn btn-marine" type="submit">Nästa</button>
         </form>
         <p class="text-center">Har du redan ett konto? <a href="{{ route('login') }}">Klicka här</a> för att logga in</p>
