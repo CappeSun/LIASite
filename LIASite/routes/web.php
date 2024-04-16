1494cd228b8412343e415155fe6e830f70e68dc8
Cappe <casper@sundegard.st>
2024-04-16 22:58:58 +0200

    Panel stuff

--- LIASite/routes/web.php
+++ LIASite/routes/web.php
@@ -42,17 +42,15 @@ Route::post('/login', [LoginController::class, 'login'])->middleware('guest');
 
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
 
-/* MATCHA */
-Route::get('/matcha', [PanelController::class, 'getList'])->name('matcha');
-
 /* PANEL */
-
+Route::get('/panels', [PanelController::class, 'getList']);
 Route::get('/panels/{panel}', [PanelController::class, 'get']);
 Route::post('/panels/create', [PanelController::class, 'create'])->middleware('auth');
 Route::patch('/panels/update', [PanelController::class, 'update'])->middleware('auth');
 Route::post('/panels/delete', [PanelController::class, 'delete'])->middleware('auth');
 Route::patch('/panels/public', [PanelController::class, 'public'])->middleware('auth');
 Route::patch('/panels/private', [PanelController::class, 'private'])->middleware('auth');
+Route::get('/matcha', [PanelController::class, 'matcha'])->name('matcha');
 
 /* ACCOUNT */
 Route::get('/account', function(){
