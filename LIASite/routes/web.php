23234aa816ecc29b324f391a81da73704feb9f8f
Cappe <casper@sundegard.st>
2024-04-17 07:09:28 +0200

    Update web.php

--- LIASite/routes/web.php
+++ LIASite/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PanelController;
 use App\Http\Controllers\UserController;
